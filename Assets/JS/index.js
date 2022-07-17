ce2af6ed3e515bcff9304a357a069c2923e7bb25
joechaaya <99142621+joechaaya@users.noreply.github.com>
2022-07-17 16:08:26 -0400

    Add files via upload
    Updated JS file to better provide search result and functionality

--- Assets/JS/index.js
+++ Assets/JS/index.js
@@ -1,90 +1,100 @@
-//unique API key generated from openweather
-var APIKey = "60f3112a35347e9ec12b982f946307b8";
-var lat = "";
-var lon = "";
-var historyArr = []
-//saves to local storage
-updateLocalStorage = function (key, data) {
-    window.localStorage.setItem(key, data)
+//Variable Declaration
+let nameEl = document.querySelector('.name');
+let tempEl = document.querySelector('.temp');
+let windEl = document.querySelector('.wind');
+let humidityEL = document.querySelector('.humidity');
+let uvEl = document.querySelector('.uv');
+let uvIndexEl = document.querySelector('.uvIndex')
+let searchBtnEl = document.querySelector('.btn-primary');
+
+// Custom API Key
+const apiKey = '60f3112a35347e9ec12b982f946307b8';
+
+
+
+function myWeather() {
+    // Open Weather API fetch request
+    let cityName = document.querySelector('#cityname').value;
+    fetch(
+        'https://api.openweathermap.org/data/2.5/weather?q=' +
+        cityName +
+        '&units=metric' +
+        '&appid=' +
+        apiKey
+    )
+    .then(function(response) {
+        return response.json();
+    })
+    .then(function(data) {
+
+        const currentDate = new Date(data.dt*1000);
+        console.log(currentDate);
+        const day = currentDate.getDate();
+        const month = currentDate.getMonth() + 1;
+        const year = currentDate.getFullYear();
+        const iconEl = document.querySelector('.icon');
+        iconEl.setAttribute("src", "https://openweathermap.org/img/wn/" + data.weather[0].icon + "@2x.png");
+        iconEl.setAttribute("alt", data.weather[0].description);
+        nameEl.innerHTML = data.name + " (" + month + "/" + day + "/" + year + ") ";
+        tempEl.innerHTML = "Temp: " + data.main.temp + " " + "&#x2103;";
+        windEl.innerHTML = "Wind: " + data.wind.speed  + " " + "MPH";
+        humidityEL.innerHTML = "Humidity: " + data.main.humidity + " " + "&#x25;" ;
+        
+        // UV index fetch request
+        let lat = data.coord.lat;
+        let lon = data.coord.lon;
+        return fetch(
+            'https://api.openweathermap.org/data/2.5/uvi/forecast?lat=' +
+            lat + '&lon=' + lon +
+            '&appid=' + apiKey + '&cnt=1'
+        )
+    })
+    .then(function(response) {
+        return response.json();
+    })
+    .then(function(data) {
+       
+        uvIndexEl.innerHTML = data[0].value
+        uvEl.innerHTML = "UV Index: ";
+        uvEl.appendChild(uvIndexEl);
+
+        // Forecast fetch request
+        return fetch(
+            'https://api.openweathermap.org/data/2.5/forecast?q=' + cityName + '&units=metric' + "&appid=" + apiKey
+        )
+    })
+    .then(function(response) {
+        return response.json();
+    })
+    .then(function(data) {
+       
+        const forecastEl = document.querySelectorAll('.forecast');
+     
+        for (i = 0; i < forecastEl.length; i++) {
+            forecastEl[i].innerHTML= "";
+            const forecastIndex = i*8 + 4;
+            const forecastDateEl = document.createElement('p');
+            const forecastIconEl = document.createElement('img');
+            const forecastTempEl = document.createElement('p');
+            const forecastWindEl = document.createElement('p');
+            const forecastHumidityEl = document.createElement('p');
+            const forecastDate = new Date(data.list[forecastIndex].dt * 1000);
+            const forecastDay = forecastDate.getDate();
+            const forecastMonth = forecastDate.getMonth() + 1;
+            const forecastYear = forecastDate.getUTCFullYear();
+            
+            forecastDateEl.innerHTML = forecastMonth + "/" + forecastDay + "/" + forecastYear;
+            forecastEl[i].append(forecastDateEl);
+            forecastIconEl.setAttribute("src", "https://openweathermap.org/img/wn/" + data.list[forecastIndex].weather[0].icon + "@2x.png");
+            forecastIconEl.setAttribute("alt", data.list[forecastIndex].weather[0].description);
+            forecastEl[i].append(forecastIconEl);
+            forecastTempEl.innerHTML = "Temp: " + (data.list[forecastIndex].main.temp) + " " + "&#x2103;";
+            forecastEl[i].append(forecastTempEl);
+            forecastWindEl.innerHTML = "Wind: " + (data.list[forecastIndex].wind.speed) + " " +  "MPH";
+            forecastEl[i].append(forecastWindEl);
+            forecastHumidityEl.innerHTML = "Humidity: " + (data.list[forecastIndex].main.humidity) + " " +  "&#x25;";
+            forecastEl[i].append(forecastHumidityEl);
+        }
+    })
 }
-//search function
-var searchWeather = function () {
-    var city = document.getElementById("city").value;
-    window.city = city
-    //updates local storage with history
-    historyArr.push(city)
-    updateLocalStorage("history", JSON.stringify(historyArr))
-
-    window.localStorage.getItem('user');
-    
-    var geoData;
-    console.log("searching weather: " + city);
-    //gets data using API + entered data to fetch weather
-    fetch("https://api.openweathermap.org/geo/1.0/direct?q=" + city + "&limit=1&appid=" + APIKey)
-        .then(response =>
-            response.json()
-        )
-        .then(data => {
-            lat = data[0].lat;
-            lon = data[0].lon;
-
-            console.log("This is the requested latitude" + lat);
-            geoData = data
-            console.log("Call getWeather");
-
-            getWeather(data);
-        }
-        )
-
-};
-var getWeather = function (geoData) {
-    console.log("Data inside get weather");
-    lat = geoData[0].lat;
-    lon = geoData[0].lon;
-    //uses coordinates to get weather data from API changed from imperial units to metric
-    fetch("https://api.openweathermap.org/data/2.5/onecall?units=metric&lat=" + lat + "&lon=" + lon + "&appid=" + APIKey)
-        .then(response =>
-            response.json()
-        )
-        .then(data => {
-            console.log(data)
-            console.log(data.current.temp)
-            console.log(data.current.wind_speed)
-
-            for (var i = 0; i <= 5; i++) { // array starts at 0
-                console.log(data.daily[i])
-                let getChild = document.getElementById("label-" + i);
-                getChild.innerHTML = "";
-
-                //remove children of card-i HERE
-                var forcast = document.getElementById("label-" + i)
-
-
-                var dateStamp = document.createElement("p")
-                dateStamp.textContent = new Date(data.daily[i].dt * 1000).toLocaleDateString()
-                forcast.append(dateStamp)
-                //Temperature data displayed in celcius
-                var pElTemp = document.createElement("p")
-                pElTemp.textContent = "temp: " + data.daily[i].temp.day + `\u00B0`
-                forcast.append(pElTemp)
-                //humidity data
-                var pElHumidity = document.createElement("p")
-                pElHumidity.textContent = `Humidity: ${data.daily[i].humidity} %`
-                forcast.append(pElHumidity)
-                //wind speed
-                var pElWind = document.createElement("p")
-                pElWind.textContent = `Wind Speed: ${data.daily[i].wind_speed} KPH`
-                forcast.append(pElWind)
-                //weather icons 
-                var imgElIcon = document.createElement("img")
-                imgElIcon.setAttribute("src", `https://openweathermap.org/img/wn/${data.daily[i].weather[0].icon}@2x.png`)
-                forcast.append(imgElIcon)
-            }
-            document.getElementById("current-city").innerHTML = (city);
-            document.getElementById("current-temperature").innerHTML = (data.current.temp + `\u00B0`);
-            document.getElementById("current-humidity").innerHTML = (data.current.humidity);
-            document.getElementById("current-wind").innerHTML = (data.current.wind_speed);
-            document.getElementById("current-UV").innerHTML = (data.current.uvi);
-        }
-        );
-};
+    searchBtnEl.addEventListener('click', myWeather)
--- README.md
+++ README.md
@@ -1,4 +1,20 @@
-# Weather-Dashboard
-When the user searches a city
-The user is presented with current weather along with the 5 day forecast
-The user is presented with the city name, the weather conditions, along with icons to represent the current weather conditions
+#  Server-Side APIs Challenge: Weather Dashboard
+
+### Challenge 6 Server-Side APIs Challenge: Weather Dashboard
+
+
+## Project Description
+This project will allow the user to search for a city's current weather and 5 day forecast
+
+## Installation
+You can clone the project from GitHub by copying either HTTPS or SSH link or by downloading the project as a zip file
+  
+
+## Screenshot
+![Screenshot]
+![CITYSEARCHHOME](citysearch.jpg)
+![CITYSEARCHRESULT](bramptonsearch.jpg)
+
+
+## Contribution
+Made by Joe Chaaya
